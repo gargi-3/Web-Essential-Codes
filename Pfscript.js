1cab396e88babdd224cef388e192f668eb3cf7d7
Gargi Rout <gargi.zr25@gmail.com>
2024-04-14 13:05:39 +0530

    third commit

---
+++ Pfscript.js
@@ -0,0 +1,7 @@
+// Pfscript.js
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // You can add code here to handle form submission, like sending data to a server or displaying a confirmation message
+    alert('Your message has been sent!');
+});
